40b50330b10c5af5be850d37724b77da80ef9a20
drgorillamd <dr_gorilla@protonmail.com>
2024-01-26 17:30:05 +0100

    feat: first tests

---
+++ test/Units/Empty.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+
+contract EmptyTest_Unit is Test {
+    function setUp() public {}
+
+    function testTest() public {
+    }
+}
