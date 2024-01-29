87bb4a865c42b4fe24ea6ee6b44245afba0fd0a4
Filip <f@filip.world>
2024-01-29 16:18:38 -0500

    Update permission IDs

--- src/libraries/JBSwapTerminalPermissionIds.sol
+++ src/libraries/JBSwapTerminalPermissionIds.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.0;
 
 library JBSwapTerminalPermissionIds {
-    uint256 public constant MODIFY_DEFAULT_POOL = 21;
-    uint256 public constant MODIFY_TWAP_PARAMS = 22;
+    // 1-20 - `JBPermissionIds`
+    // 21 - `JBHandlePermissionIds`
+    // 22-24 - `JB721PermissionIds`
+    // 25-26 - `JBBuybackPermissionIds`
+    uint256 public constant MODIFY_DEFAULT_POOL = 27;
+    uint256 public constant MODIFY_TWAP_PARAMS = 28;
 }
