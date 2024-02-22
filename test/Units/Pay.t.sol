c68af0ff10372fcc842ba694ee826951fe354d8e
drgorillamd <dr_gorilla@protonmail.com>
2024-02-22 12:07:19 +0100

    chore: test branches

--- test/Units/Pay.t.sol
+++ test/Units/Pay.t.sol
@@ -5,6 +5,54 @@ import "../helper/UnitFixture.sol";
 
 /// @notice test pay(..)
 /// @dev    tested branches:
+/// .
+/// ├── Native token in
+/// │   ├── quote
+/// │   │   ├── native token out
+/// │   │   │   └── wrap and swap
+/// │   │   │       ├── success and unwrap
+/// │   │   │       └── max slippage
+/// │   │   └── erc20 token out
+/// │   │       └── wrap and swap
+/// │   │           ├── success
+/// │   │           └── max slippage
+/// │   └── TWAP
+/// │       ├── native token out
+/// │       │   └── wrap and swap
+/// │       │       ├── success and unwrap
+/// │       │       └── max slippage
+/// │       └── erc20 token out
+/// │           └── wrap and swap
+/// │               ├── success
+/// │               └── max slippage
+/// └── ERC20 in
+///     ├── quote
+///     │   ├── native token out
+///     │   │   ├── allowance fail
+///     │   │   ├── permit2 fail
+///     │   │   └── swap
+///     │   │       ├── success and unwrap
+///     │   │       └── max slippage
+///     │   └── ERC20 out
+///     │       ├── allowance fail
+///     │       ├── permit2 fail
+///     │       └── swap
+///     │           ├── success
+///     │           └── max slippage
+///     └── twap
+///         ├── native token out
+///         │   ├── allowance fail
+///         │   ├── permit2 fail
+///         │   └── swap
+///         │       ├── success and unwrap
+///         │       └── max slippage
+///         └── ERC20 out
+///             ├── allowance fail
+///             ├── permit2 fail
+///             └── swap
+///                 ├── success
+///                 └── max slippage
+/// Modifiers for token in (native vs erc20), quote/twap and token out (native vs erc20)
 
 contract EmptyTest_Unit is UnitFixture {
     function setUp() public override {
