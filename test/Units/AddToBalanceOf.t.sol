8abc256cca1d006912f5e206816eca47b16ff015
drgorillamd <dr_gorilla@protonmail.com>
2024-02-29 17:53:16 +0100

    feat: test add to balance of

--- test/Units/AddToBalanceOf.t.sol
+++ test/Units/AddToBalanceOf.t.sol
@@ -4,17 +4,10 @@ pragma solidity ^0.8.17;
 import "../helper/UnitFixture.sol";
 
 contract AddToBalanceOf is UnitFixture {
-    address sender;
-
-    function setUp() public override {
-        super.setUp();
-
-        sender = makeAddr("sender");
-    }
-
+    /// @notice Test that addToBalanceOf reverts when called
     function test_AddToBalanceOfRevertWhen_Called() external {
-        vm.skip(true);
-
         // it should revert
+        vm.expectRevert(JBSwapTerminal.UNSUPPORTED.selector);
+        swapTerminal.addToBalanceOf(1, address(123), 1, true, "", "");
     }
 }
