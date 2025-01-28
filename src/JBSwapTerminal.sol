a44b7004e67be42a2dac8f4e7a6950ca4e23ec4f
mejango <mejango@protonmail.com>
2025-01-28 10:23:47 -0300

    cantina-chris 5/4

--- src/JBSwapTerminal.sol
+++ src/JBSwapTerminal.sol
@@ -238,19 +238,19 @@ contract JBSwapTerminal is
         // Keep a reference to the default tokens that have a known context.
         address[] memory genericContextTokens = _tokensWithAContext[DEFAULT_PROJECT_ID];
 
-        // Combine the two.
-        contexts = new JBAccountingContext[](projectContextTokens.length + genericContextTokens.length);
-
         // Keep a reference to the number of project-specific contexts.
         uint256 numberOfProjectContextTokens = projectContextTokens.length;
+
+        // Keep a reference to the number of generic contexts.
+        uint256 numberOfGenericContextTokens = genericContextTokens.length;
+
+        // Combine the two.
+        contexts = new JBAccountingContext[](numberOfProjectContextTokens + numberOfGenericContextTokens);
 
         // include all the project specific contexts
         for (uint256 i; i < numberOfProjectContextTokens; i++) {
             contexts[i] = _accountingContextFor[projectId][projectContextTokens[i]];
         }
-
-        // Keep a reference to the number of generic contexts.
-        uint256 numberOfGenericContextTokens = genericContextTokens.length;
 
         // Keep a reference to the number of combined token contexts.
         uint256 numberOfCombinedContextTokens = numberOfProjectContextTokens;
@@ -493,6 +493,11 @@ contract JBSwapTerminal is
             );
         }
 
+        // Store the token as having an accounting context.
+        if (_poolFor[projectId][token] == address(0)) {
+            _tokensWithAContext[projectId].push(token);
+        }
+
         // Update the project's pool for the token.
         _poolFor[projectId][token] = pool;
 
@@ -502,9 +507,6 @@ contract JBSwapTerminal is
             decimals: IERC20Metadata(token).decimals(),
             currency: uint32(uint160(token))
         });
-
-        // Store the token as having an accounting context.
-        _tokensWithAContext[projectId].push(token);
     }
 
     /// @notice Accepts funds for a given project, swaps them if necessary, and adds them to the project's balance in
