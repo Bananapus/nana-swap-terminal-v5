801bf322ebf725fb12d910cee5c60a2a6aa8cc18
0xBA5ED <83727748+xBA5ED@users.noreply.github.com>
2025-07-07 17:53:06 +0200

    fix: change to named arguments

--- script/ConfigurePairs.s.sol
+++ script/ConfigurePairs.s.sol
@@ -15,7 +15,6 @@ contract ConfigurePairs is Script, Sphinx {
 
     // TODO: Set the projectId we want to configure these for. (or global?).
     uint256 projectId = 0;
-    uint256 twapWindow = 10 minutes;
 
     uint256 constant ETHEREUM_MAINNET = 1;
     uint256 constant OPTIMISM_MAINNET = 10;
@@ -45,123 +44,123 @@ contract ConfigurePairs is Script, Sphinx {
     function deploy() private sphinx {
         // Configure pairs for the swap terminal.
         // DAI/ETH (0.05%)
-        configurePairFor(
-            ETHEREUM_MAINNET,
-            0x6B175474E89094C44Da98b954EedeAC495271d0F,
-            IUniswapV3Pool(0x60594a405d53811d3BC4766596EFD80fd545A270),
-            10 minutes,
-            200 // 2% slippage tolerance
-        );
+        configurePairFor({
+            chainId: ETHEREUM_MAINNET,
+            token: 0x6B175474E89094C44Da98b954EedeAC495271d0F,
+            pool: IUniswapV3Pool(0x60594a405d53811d3BC4766596EFD80fd545A270),
+            twapWindow: 10 minutes,
+            slippageTolerance: 200 // 2% slippage tolerance
+        });
         // USDC/ETH (0.05%)
-        configurePairFor(
-            ETHEREUM_MAINNET,
-            0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
-            IUniswapV3Pool(0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640),
-            2 minutes,
-            100 // 1% slippage tolerance
-        );
+        configurePairFor({
+            chainId: ETHEREUM_MAINNET,
+            token: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
+            pool: IUniswapV3Pool(0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640),
+            twapWindow: 2 minutes,
+            slippageTolerance: 100 // 1% slippage tolerance
+        });
         // USDT/ETH (0.05%)
-        configurePairFor(
-            ETHEREUM_MAINNET,
-            0xdAC17F958D2ee523a2206206994597C13D831ec7,
-            IUniswapV3Pool(0x11b815efB8f581194ae79006d24E0d814B7697F6),
-            2 minutes,
-            100 // 1% slippage tolerance
-        );
+        configurePairFor({
+            chainId: ETHEREUM_MAINNET,
+            token: 0xdAC17F958D2ee523a2206206994597C13D831ec7,
+            pool: IUniswapV3Pool(0x11b815efB8f581194ae79006d24E0d814B7697F6),
+            twapWindow: 2 minutes,
+            slippageTolerance: 100 // 1% slippage tolerance
+        });
 
         // ETH/DAI (0.3%)
-        configurePairFor(
-            ARBITRUM_MAINNET,
-            0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1,
-            IUniswapV3Pool(0xA961F0473dA4864C5eD28e00FcC53a3AAb056c1b),
-            5 minutes,
-            200
-        );
+        configurePairFor({
+            chainId: ARBITRUM_MAINNET,
+            token: 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1,
+            pool: IUniswapV3Pool(0xA961F0473dA4864C5eD28e00FcC53a3AAb056c1b),
+            twapWindow: 5 minutes,
+            slippageTolerance: 200
+        });
         // ETH/USDC (0.05%)
-        configurePairFor(
-            ARBITRUM_MAINNET,
-            0xaf88d065e77c8cC2239327C5EDb3A432268e5831,
-            IUniswapV3Pool(0xC6962004f452bE9203591991D15f6b388e09E8D0),
-            5 minutes,
-            100
-        );
+        configurePairFor({
+            chainId: ARBITRUM_MAINNET,
+            token: 0xaf88d065e77c8cC2239327C5EDb3A432268e5831,
+            pool: IUniswapV3Pool(0xC6962004f452bE9203591991D15f6b388e09E8D0),
+            twapWindow: 5 minutes,
+            slippageTolerance: 100
+        });
         // ETH/USDT (0.05%)
-        configurePairFor(
-            ARBITRUM_MAINNET,
-            0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9,
-            IUniswapV3Pool(0x641C00A822e8b671738d32a431a4Fb6074E5c79d),
-            5 minutes,
-            100
-        );
+        configurePairFor({
+            chainId: ARBITRUM_MAINNET,
+            token: 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9,
+            pool: IUniswapV3Pool(0x641C00A822e8b671738d32a431a4Fb6074E5c79d),
+            twapWindow: 5 minutes,
+            slippageTolerance: 100
+        });
 
         // ETH/DAI (0.3%)
-        configurePairFor(
-            OPTIMISM_MAINNET,
-            0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1,
-            IUniswapV3Pool(0x03aF20bDAaFfB4cC0A521796a223f7D85e2aAc31),
-            30 minutes,
-            200 // 2% slippage tolerance
-        );
+        configurePairFor({
+            chainId: OPTIMISM_MAINNET,
+            token: 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1,
+            pool: IUniswapV3Pool(0x03aF20bDAaFfB4cC0A521796a223f7D85e2aAc31),
+            twapWindow: 30 minutes,
+            slippageTolerance: 200 // 2% slippage tolerance
+        });
         // USDC/ETH (0.05%)
-        configurePairFor(
-            OPTIMISM_MAINNET,
-            0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85,
-            IUniswapV3Pool(0x1fb3cf6e48F1E7B10213E7b6d87D4c073C7Fdb7b),
-            30 minutes,
-            200 // 2% slippage tolerance
-        );
+        configurePairFor({
+            chainId: OPTIMISM_MAINNET,
+            token: 0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85,
+            pool: IUniswapV3Pool(0x1fb3cf6e48F1E7B10213E7b6d87D4c073C7Fdb7b),
+            twapWindow: 30 minutes,
+            slippageTolerance: 200 // 2% slippage tolerance
+        });
         // USDT/ETH (0.05%)
-        configurePairFor(
-            OPTIMISM_MAINNET,
-            0x94b008aA00579c1307B0EF2c499aD98a8ce58e58,
-            IUniswapV3Pool(0xc858A329Bf053BE78D6239C4A4343B8FbD21472b),
-            30 minutes,
-            200 // 2% slippage tolerance
-        );
+        configurePairFor({
+            chainId: OPTIMISM_MAINNET,
+            token: 0x94b008aA00579c1307B0EF2c499aD98a8ce58e58,
+            pool: IUniswapV3Pool(0xc858A329Bf053BE78D6239C4A4343B8FbD21472b),
+            twapWindow: 30 minutes,
+            slippageTolerance: 200 // 2% slippage tolerance
+        });
 
         // ETH/USDC (0.05%)
-        configurePairFor(
-            BASE_MAINNET,
-            0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,
-            IUniswapV3Pool(0xd0b53D9277642d899DF5C87A3966A349A798F224),
-            2 minutes,
-            100 // 1% slippage tolerance
-        );
+        configurePairFor({
+            chainId: BASE_MAINNET,
+            token: 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913,
+            pool: IUniswapV3Pool(0xd0b53D9277642d899DF5C87A3966A349A798F224),
+            twapWindow: 2 minutes,
+            slippageTolerance: 100 // 1% slippage tolerance
+        });
 
         // Testnet pairs.
         // USDC/ETH (0.3%)
-        configurePairFor(
-            ETHEREUM_SEPOLIA,
-            0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238,
-            IUniswapV3Pool(0xC31a3878E3B0739866F8fC52b97Ae9611aBe427c),
-            2 minutes,
-            500 // 5% slippage tolerance
-        );
+        configurePairFor({
+            chainId: ETHEREUM_SEPOLIA,
+            token: 0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238,
+            pool: IUniswapV3Pool(0xC31a3878E3B0739866F8fC52b97Ae9611aBe427c),
+            twapWindow: 2 minutes,
+            slippageTolerance: 500 // 5% slippage tolerance
+        });
 
         // USDC/ETH (0.3%)
-        configurePairFor(
-            BASE_SEPOLIA,
-            0x036CbD53842c5426634e7929541eC2318f3dCF7e,
-            IUniswapV3Pool(0x46880b404CD35c165EDdefF7421019F8dD25F4Ad),
-            2 minutes,
-            500 // 5% slippage tolerance
-        );
+        configurePairFor({
+            chainId: BASE_SEPOLIA,
+            token: 0x036CbD53842c5426634e7929541eC2318f3dCF7e,
+            pool: IUniswapV3Pool(0x46880b404CD35c165EDdefF7421019F8dD25F4Ad),
+            twapWindow: 2 minutes,
+            slippageTolerance: 500 // 5% slippage tolerance
+        });
 
-        configurePairFor(
-            OPTIMISM_SEPOLIA,
-            0x5fd84259d66Cd46123540766Be93DFE6D43130D7,
-            IUniswapV3Pool(0x8955C97261722d87D83D00708Bbe5f6B5b4477d6),
-            2 minutes,
-            500 // 5% slippage tolerance
-        );
+        configurePairFor({
+            chainId: OPTIMISM_SEPOLIA,
+            token: 0x5fd84259d66Cd46123540766Be93DFE6D43130D7,
+            pool: IUniswapV3Pool(0x8955C97261722d87D83D00708Bbe5f6B5b4477d6),
+            twapWindow: 2 minutes,
+            slippageTolerance: 500 // 5% slippage tolerance
+        });
 
-        configurePairFor(
-            ARBITRUM_SEPOLIA,
-            0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d,
-            IUniswapV3Pool(0x66EEAB70aC52459Dd74C6AD50D578Ef76a441bbf),
-            2 minutes,
-            500 // 5% slippage tolerance
-        );
+        configurePairFor({
+            chainId: ARBITRUM_SEPOLIA,
+            token: 0x75faf114eafb1BDbe2F0316DF893fd58CE46AA4d,
+            pool: IUniswapV3Pool(0x66EEAB70aC52459Dd74C6AD50D578Ef76a441bbf),
+            twapWindow: 2 minutes,
+            slippageTolerance: 500 // 5% slippage tolerance
+        });
     }
 
     function configurePairFor(
@@ -189,7 +188,12 @@ contract ConfigurePairs is Script, Sphinx {
         }
 
         // Add the pair to the swap terminal.
-        swapTerminal.swap_terminal.addDefaultPool(projectId, token, pool);
-        swapTerminal.swap_terminal.addTwapParamsFor(projectId, pool, twapWindow, slippageTolerance);
+        swapTerminal.swap_terminal.addDefaultPool({projectId: projectId, token: token, pool: pool});
+        swapTerminal.swap_terminal.addTwapParamsFor({
+            projectId: projectId,
+            pool: pool,
+            secondsAgo: twapWindow,
+            slippageTolerance: slippageTolerance
+        });
     }
 }
